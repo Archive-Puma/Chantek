63242fe0b4c822ad879b5a565eed8fd858171078
Cosas de Puma <kikefontanlorenzo@gmail.com>
2018-06-21 09:55:54 +0200

    Ook Module!

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 'use strict'
+
 class Brainfuck {
   constructor () {
     this.reload()
@@ -39,6 +40,7 @@ class Brainfuck {
       } break
       case '[': this.stack.push(this.instruction_pointer); break
       case ']': if (this.memory[this.index] === 0) this.stack.pop(); else this.instruction_pointer = this.stack[this.stack.length - 1]; break
+      default: break
     }
   }
 
@@ -48,6 +50,83 @@ class Brainfuck {
       this.instruction_pointer++
     }
     return this.stdout
+  }
+
+  run (code) {
+    let __code = code || this.code
+    this.reload(this.clean(__code))
+    return this.interpret()
+  }
+
+  runFile (file) {
+    let __code = require('fs').readFileSync(file, 'utf-8', function (err, data) {
+      if (err) throw err
+    })
+    return this.run(__code)
+  }
+
+  performance (code) {
+    if (code) this.reload(code)
+    console.time('Compiled in')
+    this.interpret()
+    console.timeEnd('Compiled in')
+  }
+
+  toOok (code) {
+    // TODO: Code or file with code
+    let __code = code || this.code
+    return this.clean(__code)
+      .replace(new RegExp('\\.', 'g'), 'Ook! Ook. ')
+      .replace(new RegExp('\\,', 'g'), 'Ook. Ook! ')
+      .replace(new RegExp('\\>', 'g'), 'Ook. Ook? ')
+      .replace(new RegExp('\\<', 'g'), 'Ook? Ook. ')
+      .replace(new RegExp('\\[', 'g'), 'Ook! Ook? ')
+      .replace(new RegExp('\\]', 'g'), 'Ook? Ook! ')
+      .replace(new RegExp('\\+', 'g'), 'Ook. Ook. ')
+      .replace(new RegExp('\\-', 'g'), 'Ook! Ook! ')
+      .trim()
+  }
+}
+
+class HQ9 {
+  constructor () {
+    this.reload()
+  }
+
+  reload (code) {
+    this.code = code || ''
+    this.accumulator = 0
+  }
+
+  clean (code) {
+    let __code = code || this.code
+    return __code
+      .replace(new RegExp('[^HQ9+]', 'g'), '')
+  }
+
+  parse (cmd) {
+    let ret = ''
+    switch (cmd) {
+      case 'H': ret = 'Hello world!'; break
+      case 'Q': ret = this.code; break
+      case '9':
+        for(let i = 99; i > 1; i--)
+          ret += `${i} bottles of beer on the wall, ${i} bottles of beer.\n
+          Take one down and pass it around, ${i - 1} bottles of beer on the wall.\n\n`
+        ret += `1 bottle of beer on the wall, 1 bottle of beer.\n
+        Take one down and pass it around, no more bottles of beer on the wall.\n\n
+        No more bottles of beer on the wall, no more bottles of beer.\n
+        Go to the store and buy some more, 99 bottles of beer on the wall.`
+        break
+      case '+': this.accumulator++; break
+      default: break
+    }
+    return ret
+  }
+
+  interpret () {
+    for(let cmd of this.code)
+      console.log(cmd)
   }
 
   run (code) {
@@ -133,6 +212,7 @@ class Ook {
       } break
       case '!?': this.stack.push(this.instruction_pointer); break
       case '?!': if (this.memory[this.index] === 0) this.stack.pop(); else this.instruction_pointer = this.stack[this.stack.length - 1]; break
+      default: break
     }
   }
 
@@ -187,6 +267,7 @@ class Ook {
 }
 
 module.exports = {
+  Brainfuck: new Brainfuck(),
+  HQ9: new HQ9()
   Ook: new Ook(),
-  Brainfuck: new Brainfuck()
 }
--- test/test.js
+++ test/test.js
@@ -1,8 +1,9 @@
-const { Ook, Brainfuck } = require('../src/index')
+const { Ook, Brainfuck, HQ9 } = require('../src/index')
 
 const Code = {
+  'HQ9': '9',
   'Brainfuck': '++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.',
   'Ook': 'Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook! Ook? Ook. Ook? Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook? Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook? Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook? Ook. Ook. Ook? Ook. Ook? Ook. Ook? Ook. Ook? Ook. Ook! Ook! Ook? Ook! Ook. Ook? Ook. Ook. Ook. Ook. Ook! Ook. Ook. Ook? Ook. Ook. Ook! Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook! Ook. Ook! Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook! Ook. Ook. Ook? Ook. Ook. Ook. Ook. Ook! Ook. Ook? Ook. Ook? Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook! Ook. Ook. Ook? Ook! Ook. Ook. Ook. Ook. Ook. Ook. Ook. Ook! Ook. Ook! Ook! Ook! Ook! Ook! Ook! Ook! Ook! Ook! Ook! Ook! Ook! Ook! Ook. Ook! Ook! Ook! Ook! Ook! Ook! Ook! Ook! Ook! Ook! Ook! Ook! Ook! Ook! Ook! Ook! Ook! Ook. Ook. Ook? Ook. Ook. Ook! Ook. Ook. Ook? Ook! Ook.'
 }
 
-console.log(Ook.performance(Code.Ook))
+console.log(HQ9.run(Code.HQ9))
