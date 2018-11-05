1b4f76ecfae2ba31d7d168dcfae426f9e8484f16
Sherlock Puma <kikefontanlorenzo@gmail.com>
2018-11-05 10:43:12 +0100

    :beers: HQ9+ 99 Bottles of beer on the wall!

--- HQ9+/src/Evaluator.hs
+++ HQ9+/src/Evaluator.hs
@@ -3,6 +3,7 @@ module HQ9.Evaluator (runEval) where
 import HQ9.Parser (Instructions(HelloWorld, Quine, Bottles, Increment))
 
 import System.Exit (exitWith, ExitCode(ExitSuccess))
+import Control.Monad (forM_)
 
 -- https://stackoverflow.com/questions/6270324/in-haskell-how-do-you-trim-whitespace-from-the-beginning-and-end-of-a-string
 import Data.Char (isSpace)
@@ -10,25 +11,25 @@ trim :: String -> String
 trim = f . f
    where f = reverse . dropWhile isSpace
 
+bottles 0 = do
+  putStrLn "No more bottles of beer on the wall, no more bottles of beer."
+  putStrLn "Go to the store and buy some more, 99 bottles of beer on the wall."
+bottles 1 = do
+  putStrLn "1 bottle of beer on the wall, 1 bottle of beer."
+  putStrLn "Take one down and pass it around, no more bottles of beer on the wall."
+  putStrLn ""
+  bottles 0
+bottles n = do
+  putStrLn $ show n ++ " bottles of beer on the wall, " ++ show n ++ " bottles of beer."
+  putStrLn $ "Take one down and pass it around, " ++ show (n - 1) ++ " bottles of beer on the wall."
+  putStrLn ""
+  bottles $ n - 1
 
--- TODO: 99 Bottles
+runEval instructions source accumulator = forM_ instructions $ \ins -> case ins of
+  HelloWorld -> putStrLn "Hello World!"
+  Quine -> putStrLn $ trim source
+  Bottles -> bottles 99
+  Increment -> putStrLn "Increment"
 
 
-runEval [] _ _ = exitWith ExitSuccess
-
-runEval (HelloWorld:rest) source accumulator = do
-  putStrLn "Hello World!"
-  runEval rest source accumulator
-
-runEval (Quine:rest) source accumulator = do
-  putStrLn $ trim source
-  runEval rest source accumulator
-
-runEval (Bottles:rest) source accumulator = do
-  -- TODO: 99 Bottles
-  runEval rest source accumulator
-
-runEval (Increment:rest) source accumulator =
-  runEval rest source (accumulator + 1)
-
-runEval (_:rest) source accumulator = runEval rest source accumulator
+-- TODO: Accumulator
